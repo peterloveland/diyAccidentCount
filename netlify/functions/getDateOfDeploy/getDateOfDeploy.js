115ffb8f2532b4d6d7791cd05d38d211e3f67dfd
Peter Loveland <peterloveland@uk.ibm.com>
2021-09-28 14:55:59 +0100

    update function

--- netlify/functions/getDateOfDeploy/getDateOfDeploy.js
+++ netlify/functions/getDateOfDeploy/getDateOfDeploy.js
@@ -1,15 +1,42 @@
-
-
 const fetch = require("node-fetch");
 
-const API_ENDPOINT = "https://api.netlify.com/api/v1/sites?";
 
 exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
-    .then((response) => response.json())
-    .then((data) => ({
-      statusCode: 200,
-      body: JSON.stringify(data),
-    }))
-    .catch((error) => ({ statusCode: 422, body: String(error) }));
+  const { token } = process.env;
+  const { identity, user } = context.clientContext;
+  const usersUrl = `https://api.netlify.com/api/v1/sites/dd3c7e8b-8be1-4352-9e8d-1df6d336edbe/builds`;
+  console.log('event', JSON.stringify(event));
+  console.log('context', JSON.stringify(context));
+  const adminAuthHeader = 'Bearer ' + token;
+  let data
+  try {
+    data = await fetch(usersUrl, {
+      method: 'GET',
+      headers: {
+        Authorization: adminAuthHeader
+      },
+    }).then((res) => res.json())
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: e.message
+      })
+    }
+  }
+  var date1 = Date(  JSON.stringify(data[0].created_at)  );
+  var date2 = Date.now();
+
+  date1 = new Date(date1).getTime()
+  date2 = new Date(date2).getTime()
+
+  console.log(date1)
+  var Difference_In_Time = date2 - date1;
+  var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify ( new Date(Difference_In_Days).getTime() )
+  }
 };
