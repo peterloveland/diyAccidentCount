0b034bda61ca330752bd320ba99b7315263faa9e
Peter Loveland <peterloveland@uk.ibm.com>
2021-09-28 13:28:55 +0100

    update

--- netlify/functions/getDateOfDeploy/getDateOfDeploy.js
+++ netlify/functions/getDateOfDeploy/getDateOfDeploy.js
@@ -1,21 +1,13 @@
-// const fetch = require('node-fetch')
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
-const API_ENDPOINT = 'https://api.netlify.com/api/v1/sites?';
+const API_ENDPOINT = "https://icanhazdadjoke.com/";
 
-const handler = async (event, context) => {
-  try {
-    const response = await fetch(API_ENDPOINT);
-    console.log("SUCCESS")
-    const data = await response.json();
-    return { statusCode: 200, body: JSON.stringify({ data }) };
-  } catch (error) {
-    console.log("ERROR: " + error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed fetching data' })
-    };
-  }
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
+    .then((response) => response.json())
+    .then((data) => ({
+      statusCode: 200,
+      body: data.joke,
+    }))
+    .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
-
-module.exports = { handler }
