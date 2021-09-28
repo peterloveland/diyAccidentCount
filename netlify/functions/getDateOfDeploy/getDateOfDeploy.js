1ad245aadf6f57202d8172599cfe4cfc0b29b038
Peter Loveland <peterloveland@uk.ibm.com>
2021-09-28 13:13:56 +0100

    dasdasd

--- netlify/functions/getDateOfDeploy/getDateOfDeploy.js
+++ netlify/functions/getDateOfDeploy/getDateOfDeploy.js
@@ -6,10 +6,11 @@ const API_ENDPOINT = 'https://api.netlify.com/api/v1/sites?';
 const handler = async (event, context) => {
   try {
     const response = await fetch(API_ENDPOINT);
+    console.log("SUCCESS")
     const data = await response.json();
     return { statusCode: 200, body: JSON.stringify({ data }) };
   } catch (error) {
-    console.log(error);
+    console.log("ERROR: " + error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Failed fetching data' })
