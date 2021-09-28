48382ffd8c57e8487facdb7cb03cfd6249b1a30b
Peter Loveland <peterloveland@uk.ibm.com>
2021-09-28 13:41:56 +0100

    update function

--- netlify/functions/getDateOfDeploy/getDateOfDeploy.js
+++ netlify/functions/getDateOfDeploy/getDateOfDeploy.js
@@ -1,13 +1,15 @@
+
+
 const fetch = require("node-fetch");
 
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
+const API_ENDPOINT = "https://api.netlify.com/api/v1/sites?";
 
 exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
     .then((response) => response.json())
     .then((data) => ({
       statusCode: 200,
-      body: data.joke,
+      body: JSON.stringify(data),
     }))
     .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
